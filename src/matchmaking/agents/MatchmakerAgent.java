0c5f7b09b6023b8a6b2f2d1e0fcbe6b6f6da1e43
mammalofski <masahati.navid@gmail.com>
2020-11-17 21:10:53 +0330

    successfully connet to db

--- src/matchmaking/agents/MatchmakerAgent.java
+++ src/matchmaking/agents/MatchmakerAgent.java
@@ -1,29 +1,100 @@
 package matchmaking.agents;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.OneShotBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
+import matchmaking.orm.DataBase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MatchmakerAgent extends Agent {
+	Connection conn = DataBase.getConnection();
 
 	protected void setup() {
-		//System.out.println("Hello World! My name is " + getLocalName());
+		System.out.println("starting to connecto to db");
+		try {
+			Statement statement = conn.createStatement();
+			System.out.println("created statement");
+			statement.setQueryTimeout(30); // set timeout to 30 sec.
+			
+
+			statement.executeUpdate("drop table if exists person");
+			statement.executeUpdate("create table person (id integer, name string)");
+			statement.executeUpdate("insert into person values(1, 'leo')");
+			statement.executeUpdate("insert into person values(2, 'yui')");
+			System.out.println("after executing stuff");
+			ResultSet rs = statement.executeQuery("select * from person");
+			while (rs.next()) {
+				// read the result set
+				System.out.println("name = " + rs.getString("name"));
+				System.out.println("id = " + rs.getInt("id"));
+			}
+		} catch (SQLException e) {
+			// if the error message is "out of memory",
+			// it probably means no database file is found
+			System.out.println("in catch");
+			System.out.println(e);
+			System.err.println(e.getMessage());
+		} finally {
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				// connection close failed.
+				System.err.println(e.getMessage());
+			}
+		}
+
+		System.out.println("in agent matchmaker's setup");
+		// System.out.println("Hello World! My name is " + getLocalName());
 		addBehaviour(new OneShotBehaviour() {
+
 			@Override
 			public void action() {
+				System.out.println("in agent matchmaker");
 				// TODO Auto-generated method stub
 				ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
 				msg.setContent("send");
 				msg.addReceiver(new AID("ProjectManagerAgent", AID.ISLOCALNAME));
 				send(msg);
+				System.out.println("sent the message in matchmaker agent");
+
+				Connection conn = null;
+				try {
+					// db parameters
+					String url = "jdbc:sqlite:/home/mammalofski/eclipse/SENG-696-Research-Matchmaking/sql.db";
+					// create a connection to the database
+					conn = DriverManager.getConnection(url);
+
+					System.out.println("Connection to SQLite has been established.");
+
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				} finally {
+					try {
+						if (conn != null) {
+							conn.close();
+						}
+					} catch (SQLException ex) {
+						System.out.println(ex.getMessage());
+					}
+				}
 			}
 		}
 
 		);
 		// Make this agent terminate
-		//doDelete();
+		// doDelete();
 	}
-	  
-	}
+
+}
--- src/matchmaking/agents/ProjectManagerAgent.java
+++ src/matchmaking/agents/ProjectManagerAgent.java
@@ -11,14 +11,18 @@ import jade.lang.acl.MessageTemplate;
 
 public class ProjectManagerAgent extends Agent {
 	protected void setup() {
+		System.out.println("in agent projectManager's setup");
 		//System.out.println("Hello World! My name is " + getLocalName());
 		addBehaviour(new CyclicBehaviour() {
 			@Override
 			public void action() {
 				// TODO Auto-generated method stub
+				System.out.println("in agent project");
 				ACLMessage msg = receive();
 				if (msg != null) {
 					JOptionPane.showMessageDialog(null, "Message Recived:  " + msg.getContent());
+					System.out.println("receiving the message in proeject manager");
+					System.out.println(msg.getContent());
 				} else {
 					block();
 				}
