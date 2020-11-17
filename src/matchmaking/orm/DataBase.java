f062c35d41bebc83c12f50ae8cb8adf95d8639ff
mammalofski <masahati.navid@gmail.com>
2020-11-17 21:12:32 +0330

    add DataBase file

---
+++ src/matchmaking/orm/DataBase.java
@@ -0,0 +1,62 @@
+package matchmaking.orm;
+//package net.sqlitetutorial;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBase {
+	/**
+	 * Connect to a sample database
+	 */
+//	private static DataBase single_instance = null;
+	private static Connection conn = null;
+
+	private DataBase() {
+		createConnection();
+	}
+	
+	public static Connection createConnection() {
+		Connection connection = null;
+
+		try {
+			// db parameters
+			String url = "jdbc:sqlite:/home/mammalofski/eclipse/SENG-696-Research-Matchmaking/mysqlite.db";
+			// create a connection to the database
+			connection = DriverManager.getConnection(url);
+
+			System.out.println("Connection to SQLite has been established.");
+
+		} catch (SQLException e) {
+			System.out.println("in Dataase's catch");
+			System.out.println(e.getMessage());
+		} finally {
+			System.out.println("in finally");
+			System.out.println(connection);
+//			try {
+//				if (connection != null) {
+//					connection.close();
+//				}
+//			} catch (SQLException ex) {
+//				System.out.println("in Dataase's catch 2");
+//				System.out.println(ex.getMessage());
+//			}
+		}
+		System.out.println("returning conn");
+		System.out.println(connection);
+		return connection;
+	}
+
+	public static Connection getConnection() {
+		if (conn == null) 
+			conn = createConnection();
+		return conn;
+	}
+
+	/**
+	 * @param args the command line arguments
+	 */
+//	public static void main(String[] args) {
+//		connect();
+//	}
+}
